f1b40a3a45497357b09ace54d404e187fe47e080
Max Nordlund <max.nordlund@gmail.com>
2013-09-16 11:06:01 +0200

    Started work on rows and matrix classes

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python3
 
-class MetaPickler(type):
-  "Metaclass for gnosis.xml.pickle serialization"
-  def __init__(cls, name, bases, dict):
-    from gnosis.xml.pickle import dumps
-    super(MetaPickler, cls).__init__(name, bases, dict)
-    setattr(cls, 'dumps', dumps)
+class RowMethod:
+  "This represents a method on a row"
+  __slots__ = ("name")
 
+  def __init__(this, name):
+    this.name = name
 
-class Row(list):
-  __slots__ = ("data")
-
-  def __init__(self, data):
-    self.data = data
-  # TODO: Add metaclass to tidy up arithmetic methods,
-  # maybe us inheritance from list for common stuff
-
-  def __iadd__(self, other):
+  def __call__(this, self, other):
+    "This is this method object, self is the row this method is being called"
+    "on and other is the second parameter to this overloaded method."
     if isinstance(other, Row):
       if len(self) == len(other):
         for index, value in enumerate(other):
-          # self.data[index].__iadd__(value)
-          self.data[index] += value
+          # self.data[index] += value
+          getattr(self.data[index], this.name)(value)
         else:
           raise TypeError("Rows must be of equal length to add")
       else:
         for index, value in enumerate(self):
-          # self.data[index].__iadd__(other)
-          self.data[index] += other
+          # self.data[index] += other
+          getattr(self.data[index], this.name)(other)
+
+class MetaRow(type):
+  "Metaclass for matrix rows"
+  def __init__(cls, name, bases, dict):
+    super(MetaRow, cls).__init__(name, bases, dict)
+    methods = ("add", "sub", "mul", "truediv")
+    for name in methods:
+      for modifier in ("__%s__", "__r%s__", "__i%s__"):
+        method = RowMethod(modifier % name)
+        setattr(cls, method.name, method)
+
+class Row(list):
+  "This represents a single row of a matrix"
+  __metaclass__ = MetaRow
+  __slots__ = ("data")
+
+  def __init__(self, data):
+    self.data = data
 
